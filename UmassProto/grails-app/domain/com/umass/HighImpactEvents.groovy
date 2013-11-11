db547f40ffbbf72c6d69a3b0459edcd0c6b06144
Josh Westbrook <josh@davinciconcepts.com>
2013-11-11 16:15:23 -0500

    new classes

---
+++ UmassProto/grails-app/domain/com/umass/HighImpactEvents.groovy
@@ -0,0 +1,17 @@
+package com.umass
+
+class HighImpactEvents {
+
+    String description
+    String impaces
+    Date startDate
+    Date endDate
+    String county
+    String state
+    String networksOperational
+    String sensorNotOperational
+    String links
+
+    static constraints = {
+    }
+}
---
+++ UmassProto/grails-app/domain/com/umass/SiteSensorInSitu.groovy
@@ -0,0 +1,26 @@
+package com.umass
+
+class SiteSensorInSitu {
+
+    Contact contact
+    String location
+    String latitude
+    String longitude
+    String county
+    String state
+    String heightMsl
+    String calibrationConstants
+    String methodOfCalibration
+    String dataTransmissionFrequency
+    Date lastSensorReplacement
+    Date maintenanceDate
+    String periodOfDowntimeForMaintenance
+    Date calibrationDate
+    Date installationDate
+    Date startDate
+    Date endDate
+    Date decommissionDate
+
+    static constraints = {
+    }
+}
